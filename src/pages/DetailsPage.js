2a1ec7c08f0be44a4faf9c6f26e559247f9751af
= <adedejifavour6@gmail.com>
2022-11-24 15:32:51 +0100

    Add more functionality for Detail Page

--- src/pages/DetailsPage.js
+++ src/pages/DetailsPage.js
@@ -1,43 +1,31 @@
+/* eslint-disable max-len */
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import Navbar from './Navbar';
 import Heading from '../component/heading';
+import DetailComponent from '../component/detailComponent';
+
+import './DetailsPage.css';
 
 const DetailsPage = () => {
   const { id: target } = useParams();
-  const covidData = useSelector((state) => state.metrics);
-  const targetCountry = covidData.covid19Data.find((data) => data.Country === target);
+  const covidDatas = useSelector((state) => state.metrics);
+  const targetCountry = covidDatas && covidDatas.covid19Data.find((data) => data.Country === target);
 
   return (
     <>
       <Navbar navHeading="Countries Covid19 Details" />
-      <Heading name={target} confirmedCases={targetCountry.TotalCases} />
-      <section>
-        <p>
-          {`${target} covid19 detailed Information`}
-        </p>
-        <div>
-          <p>
-            {`Population: ${targetCountry.Population}`}
-          </p>
-          <p>
-            {`Total Cases: ${targetCountry.TotalCases}`}
-          </p>
-          <p>
-            {`Total Recovery: ${targetCountry.TotalRecovered}`}
-          </p>
-          <p>
-            {`Total Death: ${targetCountry.TotalDeaths}`}
-          </p>
-          <p>
-            {`Active Cases: ${targetCountry.ActiveCases}`}
-          </p>
-          <p>
-            {`Total Tests: ${targetCountry.TotalTests}`}
-          </p>
-        </div>
-      </section>
+      <Heading name={target} confirmedCases={targetCountry && targetCountry.TotalCases} />
+      <DetailComponent
+        target={target}
+        Population={targetCountry && targetCountry.Population}
+        TotalRecovered={targetCountry && targetCountry.TotalRecovered}
+        TotalTests={targetCountry && targetCountry.TotalTests}
+        TotalCases={targetCountry && targetCountry.TotalCases}
+        ActiveCases={targetCountry && targetCountry.ActiveCases}
+        TotalDeaths={targetCountry && targetCountry.TotalDeaths}
+      />
     </>
   );
 };
