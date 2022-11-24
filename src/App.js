f11c1b5f96f3ab283ab62fb75b734a42bb2cf472
= <adedejifavour6@gmail.com>
2022-11-24 15:38:36 +0100

    Add little changes to app.js file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import Details from './pages/Details';
+import HomePage from './pages/HomePage';
+import ErrorPage from './pages/ErrorPage';
 import './App.css';
+import DetailsPage from './pages/DetailsPage';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/details/:id" element={<Details />} />
+      <Route path="/" element={<HomePage />} />
+      <Route path="/details/:id" element={<DetailsPage />} />
+      <Route path="*" element={<ErrorPage />} />
     </Routes>
   );
 }
