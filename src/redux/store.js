d9667c8aadd8e6a5fab1a17f6e1f9082274b8040
= <adedejifavour6@gmail.com>
2022-11-24 15:35:17 +0100

    Update redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import metricsReducer from './metrics/metricsRedux';
+import metricsReducer from './metrics/metricsSlice';
 
 export default configureStore({
   reducer: {
