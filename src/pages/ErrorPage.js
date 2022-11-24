c85c6bea17185a397504e63dcd55618268ea65c0
= <adedejifavour6@gmail.com>
2022-11-24 15:34:05 +0100

    Add error page

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const ErrorPage = () => (
-  <div>
+  <div className="error_page">
     <h2>PAGE NOT FOUND</h2>
     <p>please check the url to display your desired page</p>
   </div>
